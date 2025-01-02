9d110ce284aa372acae3c0041baa2a875b119bd7
NIKHILKAUSHIK72 <98116473+NIKHILKAUSHIK72@users.noreply.github.com>
2025-01-02 15:04:44 +0530

    Update AMBA.js

--- AMBA.js
+++ AMBA.js
@@ -28,7 +28,8 @@ const data =[
     { date: "2024-12-13",totalOrders:900,totalQty:1600,totalDispatch:1700, totalProductionPress1:2500,totalProductionPress2:2600},
    {date : "2024-12-14",totalOrders:1000,totalQty:1700,totalDispatch:1800,totalProductionPress1:2600,totalProductionPress2:2700},
     {date : "2024-12-15",totalOrders:1100,totalQty:1750,totalDispatch:1800,totalProductionPress1:2600,totalProductionPress2:2700},
-    {date : "2024-12-16",totalOrders:1200,totalQty:1750,totalDispatch:1800,totalProductionPress1:2600,totalProductionPress2:2700}
+    {date : "2024-12-16",totalOrders:1200,totalQty:1750,totalDispatch:1800,totalProductionPress1:2600,totalProductionPress2:2700},
+    {date : "2025-01-01",totalOrders:1300,totalQty:1850,totalDispatch:1900,totalProductionPress1:2700,totalProductionPress2:2800}
 ];
 
     
